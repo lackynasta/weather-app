4a00f6d1ff3abf764f4adde40983618c0b196a79
lackynasta <lackynasta@gmail.com>
2023-12-16 12:30:57 +0300

    defining fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\City;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * This code is responsible for loading city data into the database.
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $cities = ['Antananarivo', 'Antsiranana', 'Fianarantsoa', 'Mahajanga', 'Toamasina', 'Toliara'];
+        foreach ($cities as $city) {
+            $cityEntity = new City();
+            $cityEntity
+                ->setName($city)
+                ->setLabel($city);
+            $manager->persist($cityEntity);
+        }
         $manager->flush();
     }
 }
